66a1b1f8896d91f5eb0cb979bae829783a1c7e94
DoggyDoggyDoggy <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-02-02 12:21:48 +1300

    Draft Month Grid

--- app/src/main/java/denys/diomaxius/habittracker/MainActivity.kt
+++ app/src/main/java/denys/diomaxius/habittracker/MainActivity.kt
@@ -10,15 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import denys.diomaxius.habittracker.ui.HabitTable
 import denys.diomaxius.habittracker.ui.theme.HabitTrackerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HabitTrackerTheme {
-
-            }
+            HabitTable()
         }
     }
 }
---
+++ app/src/main/java/denys/diomaxius/habittracker/ui/HabitTable.kt
@@ -0,0 +1,59 @@
+package denys.diomaxius.habittracker.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
+
+@Composable
+fun HabitTable(
+
+) {
+
+    MonthsGrid(days = 31)
+
+}
+
+@Composable
+fun MonthsGrid(
+    modifier: Modifier = Modifier,
+    spacing: Int = 4,
+    days: Int
+) {
+    Layout(
+        modifier = modifier,
+        content = {
+            repeat(days) {
+                Box(
+                    modifier = Modifier
+                        .size(16.dp)
+                        .background(Color.Blue)
+                )
+            }
+        }
+    ) { measurables, constraints ->
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            var x = 0
+            var y = 0
+            var index = 1
+            val placeables = measurables.map { measurable ->
+                measurable.measure(constraints)
+            }
+
+            placeables.forEach { placeable ->
+                placeable.placeRelative(x = x, y = y)
+                if (index % 7 == 0) {
+                    x += placeable.width + spacing
+                    y = 0
+                } else {
+                    y += placeable.height + spacing
+                }
+                index++
+            }
+        }
+    }
+}
