0e9f4e1b66858046698df26c088e1cc75fc37209
DoggyDoggyDoggy <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-02-27 23:29:03 +1300

    Draft for calc streak

--- app/src/main/java/denys/diomaxius/habittracker/domain/usecase/CalcStreakUseCase.kt
+++ app/src/main/java/denys/diomaxius/habittracker/domain/usecase/CalcStreakUseCase.kt
@@ -4,6 +4,8 @@ import denys.diomaxius.habittracker.domain.model.HabitProgress
 import denys.diomaxius.habittracker.domain.repository.HabitProgressRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import java.time.LocalDate
+import java.time.temporal.ChronoUnit
 import javax.inject.Inject
 
 class CalcStreakUseCase @Inject constructor(
@@ -17,7 +19,24 @@ class CalcStreakUseCase @Inject constructor(
     }
 
     private fun calculateStreak(progressList: List<HabitProgress>) : Int {
-        TODO("Not yet implemented")
+        val completedRecords = progressList
+            .filter { it.isCompleted }
+            .sortedByDescending { it.date }
+
+        var streak = 0
+        var currentDate = LocalDate.now()
+
+        for (progress in completedRecords) {
+            val diffDays = ChronoUnit.DAYS.between(progress.date, currentDate).toInt()
+            if (diffDays == 0 || diffDays == 1) {
+                streak++
+                currentDate = progress.date
+            } else {
+                break
+            }
+        }
+
+        return streak
     }
 
 }
